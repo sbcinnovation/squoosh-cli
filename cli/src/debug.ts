fbdbec1b2daa08647b71ab290678a337bea8d224
SBC Innovation <84935503+skylovescoffee@users.noreply.github.com>
2025-09-14 23:49:16 -0400

    Begin to transition to Bun

---
+++ cli/src/debug.ts
@@ -0,0 +1,14 @@
+#!/usr/bin/env bun
+
+import path from 'path';
+
+const args = Bun.argv.slice(2);
+const __filename = Bun.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+Bun.spawn(
+  ['bun',  '--no-experimental-fetch', '--inspect', __dirname + '/index.js', ...args, ],
+  {
+    stdio: ['inherit', 'inherit', 'inherit'] 
+  }
+);
